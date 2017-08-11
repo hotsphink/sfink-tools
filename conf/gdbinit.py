f62c5bec76b40de81ffb6b2ec7b21d2efcc155e5
Steve Fink <sfink@mozilla.com>
2017-08-11 15:21:38 -0700

    Move some python code .gdbinit -> .gdbinit.py

--- conf/gdbinit.py
+++ conf/gdbinit.py
@@ -337,6 +337,25 @@ JITInstructionMap()
 
 ######################################################################
 
+# repeat "command" [limit]
+# Example: rep $->parent 20
+class Repeat(gdb.Command):
+  """Repeat command [limit] times or until an error is hit"""
+  def __init__(self):
+    gdb.Command.__init__(self, "repeat", gdb.COMMAND_USER)
+
+  def invoke(self, arg, from_tty):
+    args = gdb.string_to_argv(arg)
+    cmd = args[0]
+    limit = int(args[1]) if len(args) > 1 else 9999
+    for i in range(limit):
+      #print("%d: executing %s" % (i, cmd))
+      gdb.execute(cmd)
+
+Repeat()
+
+######################################################################
+
 if False:
     for o in gdb.objfiles():
         print("objfile: " + o.filename)
