8893171078d7108e07498505802f1d3ce1065f23
Steve Fink <sfink@mozilla.com>
2018-10-10 15:49:51 -0700

    Remove unused repeat command

--- conf/gdbinit.py
+++ conf/gdbinit.py
@@ -24,25 +24,6 @@ PythonPrint()
 
 ######################################################################
 
-# repeat "command" [limit]
-# Example: rep $->parent 20
-class Repeat(gdb.Command):
-  """Repeat command [limit] times or until an error is hit"""
-  def __init__(self):
-    gdb.Command.__init__(self, "repeat", gdb.COMMAND_USER)
-
-  def invoke(self, arg, from_tty):
-    args = gdb.string_to_argv(arg)
-    cmd = args[0]
-    limit = int(args[1]) if len(args) > 1 else 9999
-    for i in range(limit):
-      #print("%d: executing %s" % (i, cmd))
-      gdb.execute(cmd)
-
-Repeat()
-
-######################################################################
-
 # pdo expr
 # Example: pdo p data[[i for i in range(10,20)]].key
 # Example: pdo p {i for i in range(10,20)}*{j for j in [1, -1]}
