a0bc421f2f3447c13b4d944fd640a0bd41a557ed
Steve Fink <sfink@mozilla.com>
2023-08-21 16:36:31 -0700

    [gdbconf] add commented symbol server?

--- conf/gdbinit.symbols.py
+++ conf/gdbinit.symbols.py
@@ -24,7 +24,8 @@ except ImportError:
     from urllib import quote
     from urlparse import urljoin
 
-SYMBOL_SERVER_URL = 'https://symbols.mozilla.org'
+SYMBOL_SERVER_URL = 'https://s3-us-west-2.amazonaws.com/org.mozilla.crash-stats.symbols-public/v1/'
+#SYMBOL_SERVER_URL = 'https://symbolication.services.mozilla.com/symbolication/'
 
 debug_dir = os.path.join(os.environ['HOME'], '.cache', 'gdb')
 cache_dir = os.path.join(debug_dir, '.build-id')
